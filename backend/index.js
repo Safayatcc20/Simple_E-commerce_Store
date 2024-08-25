6b3eb7db77e1c6fd79b15a8de9ff8c446af2c280
Safayatcc20 <mdsafayatcc20@gmail.com>
2024-08-26 02:01:24 +0600

    safayat

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
-
+// const { error } = require("console");
 app.use(express.json());
 app.use(cors());
 
@@ -30,9 +30,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage:storage})
 
-//Creating Upload Endpoint for images
-
-app.use('/images',express.static('/upload/images'))
+//creating upload endpoint for images
+app.use('/images',express.static('upload/images'))
 
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
@@ -78,11 +77,11 @@ const Product = mongoose.model("Product",{
     },
 })
 
-app.post("/addproduct",async(req,res)=>{
-
+app.post('/addproduct',async(req,res)=>{
     let products = await Product.find({});
     let id;
-    if(products.length>0){
+    if(products.length>0)
+    {
         let last_product_array =  products.slice(-1);
         let last_product = last_product_array[0];
         id = last_product.id+1;
@@ -125,11 +124,149 @@ app.get('/allproducts',async(req,res)=>{
     res.send(products);
 })
 
-app.listen(port,(error)=>{
-    if(!error){
-        console.log("Server Running on Port "+port)
+// Schema creating for user model
+const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+ })
+
+//Creating Endpoint for registration the user
+app.post('/signup',async(req,res)=>{
+
+    let check = await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false,errors:"existing user found with same email address"})
+    }
+    let cart = {};
+    for ( let i = 0 ; i < 300;i++){
+        cart[i] = 0 ;     
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+    
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+
+    const token =  jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+
+})
+
+//Creating Endpoint for login the user
+app.post('/login',async(req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if(user){
+        const passCompare = req.body.password === user.password;
+        if(passCompare){
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+            const token =  jwt.sign(data,'secret_ecom');
+            res.json({success:true,token});
+        }
+        else{
+            res.json({success:false,errors:"Wrong Password"});
+        }
     }
     else{
-        console.log("Error : "+error)
+        res.json({success:false,errors:"Wrong Email Id"});
+    }
+})
+
+// creating endpoint for newcollection data
+app.get('/newcollections', async (req, res) => {
+    let newcollection = await Product.find({}).sort({ _id: -1 }).limit(8);
+    console.log("NewCollection Fetched");
+    res.send(newcollection);
+});
+
+
+//creating endpoint for popular in women sectiong
+app.get('/popularinwomen',async(req,res)=>{
+    let products = await Product.find({ category: "women" })
+            .sort({ _id: -1 }) 
+            .limit(4);
+
+        console.log("Popular in women fetched");
+        res.send(products);
+})
+
+//creating middleware to fetch user
+const fetchUser = async(req , res , next)=>{
+    const token = req.header('auth-token');
+    if(!token){
+        res.status(401).send({errors:"Please authenticate using valid token."});
+    }
+    else{
+        try{
+            const data = jwt.verify(token,'secret_ecom');
+            req.user = data.user;
+            next();
+        }catch(error){
+            res.status(401).send({errors:"Please  authenticate using valid token"})
+        }
+    }
+}
+
+
+//creating endpoint for adding products in cartdata
+app.post('/addtocart',fetchUser,async(req,res)=>{    
+    console.log("Added",req.body.itemId);
+    let userData = await Users.findOne({_id:req.user.id});
+    userData.cartData[req.body.itemId] +=1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Added")
+})
+
+//creating endpoint to removing from cartdata
+app.post('/removefromcart',async(req,res)=>{
+    console.log("removed",req.body.itemId);
+    let userData = await Users.findOne({_id:req.user.id});
+    if(userData.cartData[req.body.itemId]>0)
+    userData.cartData[req.body.itemId] -=1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Removed")
+})
+
+// creating endpoint to get cartdasta
+app.post('/getcart',fetchUser,async(req,res)=>{
+    console.log("GetCart");
+    let userData = await  Users.findOne({_id:req.user.id});
+    res.json(userData.cartData);
+
+})
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log("Server is running on Port "+port)
+    }
+    else{
+        console.log("Error: "+error)
     }
 })
--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,4 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './NewCollections.css'
+
+import Item from '../Item/Item';
+
+const NewCollections = () => {
+
+  const [new_collections, setNew_collection] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/newcollections')
+    .then((response)=>response.json()).then((data)=>setNew_collection(data));
+  },[])
+
+  return (
+    <div className='new-collections'>
+      <h1>New Collections</h1>
+      <hr />
+      <div className="collections">
+        {new_collections.map((item , i) => {
+          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>;
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default NewCollections
+/*import React from 'react'
 import './NewCollections.css'
 import new_collection from '../Assets/new_collections';
 import Item from '../Item/Item';
@@ -18,3 +46,8 @@ const NewCollections = () => {
 }
 
 export default NewCollections
+
+previous code
+
+
+*/
